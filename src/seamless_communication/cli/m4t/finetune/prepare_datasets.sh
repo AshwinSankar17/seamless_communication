c4051ee76e0c06a9879c3f42e4c8b99bfb5a4327
Ashwin Sankar <ashwins1211@gmail.com>
2024-12-10 04:59:21 +0000

    remove unnecessary files

--- src/seamless_communication/cli/m4t/finetune/prepare_datasets.sh
+++ src/seamless_communication/cli/m4t/finetune/prepare_datasets.sh
@@ -3,5 +3,5 @@ ALIGNMENT_THESHOLD=0.8 MINING_THRESHOLD=0.6 python bhasa_anuvaad.py \
 --direction "all" \
 --save_dir "/data/BhasaAnuvaad" \
 --hf_cache_dir "/data/huggingface/datasets/BhasaAnuvaad" \
---huggingface_token $HUGGINGFACE_TOKEN \
+--huggingface_token $HF_TOKEN \
 --do_split
